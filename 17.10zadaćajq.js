c5ed4b78eef81edc7b2280214b88c0c423a5b440
Josip S <josip.skrlecvk@gmail.com>
2023-10-17 23:54:39 +0200

    zadaća

---
+++ 17.10zadaćajq.js
@@ -0,0 +1,59 @@
+/*1. Pomoću jQueryja:
+	
+	- pratite resize događaj na korisničkom ekranu ispisujući širinu ekrana u konzolu
+	- pratite kretanje miša i na ulazak miša preko retka tablice promijenite mu pozadinsku boju u žutu
+	- na izlazak miša sa retka mora se pozadinska boja vratiti na prijašnju */
+
+$(document).ready(function () {
+  $(window).on("resize", function () {
+    var windowWidth = $(window).width();
+    console.log("Širina ekrana: " + windowWidth + "px");
+  });
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://pokeapi.co/api/v2/pokemon-color/yellow", true);
+
+  function addStripes() {
+    $("table tr").removeClass("striped");
+    $("table tr:nth-child(even)").addClass("striped");
+  }
+
+  function afterRender() {
+    $('[data-toggle="popover"]').popover();
+    $("table th").css("color", "darkBlue");
+    addStripes();
+
+    setTimeout(function () {
+      const hideElements = $("table td a:contains('p')").filter(function () {
+        return this.innerHTML.indexOf("p") == 0;
+      });
+      hideElements.closest("tr").remove();
+      addStripes();
+
+      const info = $("<div></div>")
+        .insertAfter($("#pokemon-list"))
+        .text("Skriveno: " + hideElements.length);
+    }, 2000);
+  }
+
+  function fillList() {
+    const data = JSON.parse(xhr.response);
+    const source = document.getElementById("pokemon-list").innerHTML;
+    const template = Handlebars.compile(source);
+    const context = {
+      pokemon: data.pokemon_species.slice(0, 20),
+      tableClass: "table",
+    };
+    const html = template(context);
+
+    document.getElementById("result").innerHTML = html;
+
+    afterRender();
+  }
+
+  xhr.onload = function () {
+    fillList();
+  };
+
+  xhr.send();
+});
